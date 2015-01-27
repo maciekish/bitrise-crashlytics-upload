30d54b7981920b7f483ea9e823e1946bfdfabcde
Maciej Swic <maciej@swic.name>
2015-01-27 10:54:57 +0100

    Auto-find the Crashlytics.framework/submit path

--- step.sh
+++ step.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-cmd="\"$BITRISE_SOURCE_DIR/$CRASHLYTICS_PATH\" $CRASHLYTICS_API_KEY $CRASHLYTICS_BUILD_SECRET -ipaPath \"$BITRISE_IPA_PATH\" -emails $CRASHLYTICS_EMAILS -groupAliases $CRASHLYTICS_GROUP_ALIASES -notifications $CRASHLYTICS_NOTIFICATIONS"
+#Go to the project folder
+eval cd "$BITRISE_SOURCE_DIR"
 
-eval "$cmd"
+#Check if provided path exists
+if [ ! -f "$BITRISE_SOURCE_DIR/$CRASHLYTICS_PATH" ]; then
+    export CRASHLYTICS_PATH=`find . | grep -i crashlytics.framework/submit`
+fi
+
+echo "Using $CRASHLYTICS_PATH"
+
+eval "$CRASHLYTICS_PATH $CRASHLYTICS_API_KEY $CRASHLYTICS_BUILD_SECRET -ipaPath \"$BITRISE_IPA_PATH\" -emails $CRASHLYTICS_EMAILS -groupAliases $CRASHLYTICS_GROUP_ALIASES -notifications $CRASHLYTICS_NOTIFICATIONS"
 
 exit $?
