0d6e79a08a6960a0e868d558a3136a395b23e6be
Maciej Swic <maciej@swic.name>
2015-01-27 11:21:57 +0100

    Fixed a path bug

--- step.sh
+++ step.sh
@@ -5,10 +5,8 @@ eval cd "$BITRISE_SOURCE_DIR"
 
 #Check if provided path exists
 if [ ! -f "$BITRISE_SOURCE_DIR/$CRASHLYTICS_PATH" ]; then
-    export CRASHLYTICS_PATH=`find . | grep -i crashlytics.framework/submit`
+    CRASHLYTICS_PATH=`find . | grep -i crashlytics.framework/submit | head -1`
 fi
-
-echo "Using $CRASHLYTICS_PATH"
 
 eval "$CRASHLYTICS_PATH $CRASHLYTICS_API_KEY $CRASHLYTICS_BUILD_SECRET -ipaPath \"$BITRISE_IPA_PATH\" -emails $CRASHLYTICS_EMAILS -groupAliases $CRASHLYTICS_GROUP_ALIASES -notifications $CRASHLYTICS_NOTIFICATIONS"
 
