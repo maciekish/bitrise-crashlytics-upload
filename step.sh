ae80f1b67f27ab57e927106e84947bf8dae8a768
Maciej Swic <maciej@swic.name>
2015-01-28 10:45:00 +0100

    Debug info

--- step.sh
+++ step.sh
@@ -10,6 +10,11 @@ if [ ! -f "$CRASHLYTICS_PATH" ]; then
     CRASHLYTICS_PATH=`find . | grep -i crashlytics.framework/submit | head -1`
 fi
 
+pwd
+ls -lah
+file "$CRASHLYTICS_PATH"
+file "$BITRISE_IPA_PATH"
+
 CMD="$CRASHLYTICS_PATH $CRASHLYTICS_API_KEY $CRASHLYTICS_BUILD_SECRET -ipaPath \"$BITRISE_IPA_PATH\" -emails $CRASHLYTICS_EMAILS -groupAliases $CRASHLYTICS_GROUP_ALIASES -notifications $CRASHLYTICS_NOTIFICATIONS"
 
 echo "Executing $CMD"
